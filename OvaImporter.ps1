d9ab96480353b8eae203ac9283c2ce30a8e008d7
Genosystem <genosystem@gmail.com>
2020-05-21 11:03:56 +0200

    Colors

--- OvaImporter.ps1
+++ OvaImporter.ps1
@@ -1,11 +1,9 @@
 # OVA Importer Tool
 <#
-TODO Change Colors of selected Items
 TODO Check Values
-TODO 
+TODO File default
+
 #>
-
-
 
 
 [string]$OvaSource = Read-Host "File path to OVA"
@@ -13,24 +11,24 @@ $ClustersList = get-cluster
 $i = 0
 foreach ($ClustersItem in $ClustersList ) {
     
-    Write-Host "[$i] - $ClustersItem"
+    Write-Host "[$i]    - $ClustersItem"
     $i++
 }
 
 $ClusterSelected = Get-Cluster ($ClustersList[(Read-Host "Cluster Number")])
-Write-host "$ClusterSelected Selected"
+Write-host "$ClusterSelected Selected" -ForegroundColor DarkGreen
 $RNDHost = ($ClusterSelected | Get-VMHost)
 
 $DSList = get-cluster $ClusterSelected | Get-Datastore
 $i = 0
 foreach ($DSItem in $DSList ) {
     
-    Write-Host "[$i] - $DSItem"
+    Write-Host "[$i]    - $DSItem"
     $i++
 }
 $DSSelected = Get-Datastore ($DSList[(Read-Host "DataStore Number")])
-Write-host "$DSSelected Selected"
+Write-host "$DSSelected Selected" -ForegroundColor DarkGreen
 $VMName = Read-Host “Vm Name”
 
-#TODO Don't work with mapped drives
+# Don't work with mapped drives
 Import-vApp –Source $OvaSource -Location $ClusterSelected[0] -Datastore $DSSelected -Name $VMName -Host $RNDHost[(Get-random ($RNDHost.Count))] -Force
